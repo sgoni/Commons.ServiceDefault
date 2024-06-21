28c0faa0e4b277ef83bf1f9a86c3d80a5d2ae7d0
sgoni <steven.goni@outlook.com>
2024-06-20 20:24:57 -0600

    Fix minor

--- src/Commons.ServiceDefaults/MiddlewareExtensions.cs
+++ src/Commons.ServiceDefaults/MiddlewareExtensions.cs
@@ -45,6 +45,7 @@ namespace Commons.ServiceDefaults
             builder.AddDefaultHealthChecks();
             builder.AddDefaultSerilog();
             builder.AddDefaultCors();
+            builder.Services.AddHttpContextAccessor();
             return builder;
         }
 
